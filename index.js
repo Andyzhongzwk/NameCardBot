616534f0784ff82b460ae512ec843e8fd8e2cd25
Andyzhongzwk <131367457+Andyzhongzwk@users.noreply.github.com>
2024-08-20 22:02:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const mime = require('mime-types');
+require('dotenv').config();
 const api = require('./api');
 
 const scriptStartTime = Date.now();
@@ -11,7 +12,7 @@ const client = new Client({
     authTimeoutMs: 0,
     takeoverOnConflict: 60 * 60 * 1000,
     puppeteer: { headless: false },
-    authStrategy: new LocalAuth({ clientId: "fdmt-wa-bot" }),
+    authStrategy: new LocalAuth({ clientId: process.env.WHATSAPP_CLIENT_ID }),
 });
 
 client.initialize();
@@ -31,10 +32,7 @@ client.on("message", async (msg) => {
         return;
     }
 
-    // The scenarios which will trigger the bot
-    // RM 60 Practitioner Lounge = "85260523886-1595337332@g.us"
-    // Bluebird Andy 5540 = "120363028296358990@g.us" for testing
-    if (msg.type === "image" && msg.from === "120363028296358990@g.us") {
+    if (msg.type === "image" && msg.from === process.env.PRACTITIONER_GRP_ID) {
         try {
             msg.reply("Image received, processing...");
 
@@ -45,7 +43,7 @@ client.on("message", async (msg) => {
             console.log(imageUrl);
 
             // Step 2: Get response from AIML Api
-            const responseJSON = await api.getAIMLApiResponse(imageUrl); // Get response in the format of JSON from AIML Api
+            const responseJSON = await api.getAIMLApiResponse(imageUrl);
             let { name, salutation, email, source_link, whatsapp_number, org, relation, } = responseJSON;
 
             // Step 3: Check if the provided image is a contact card
@@ -57,17 +55,16 @@ client.on("message", async (msg) => {
                 const link = await api.insertToContactTable(name, salutation, email, source_link, whatsapp_number, org, relation);
 
                 // Step 5: Reply to the WhatsApp message with retrieved information
-                name = name? name : "(Name not given)";
-                salutation = salutation? salutation : "(Salutation not given)";
-                email = email? email : "(Email not given)";
-                source_link = source_link? source_link : "(Website not given)";
-                whatsapp_number = whatsapp_number? whatsapp_number : "(Whatsapp number not given)";
-                org = org? org : "(Company/ Organization name not given)";
-                relation = relation? responseJSON.relation : "(Title not given)";
+                name = name ? name : "(Name not given)";
+                salutation = salutation ? salutation : "(Salutation not given)";
+                email = email ? email : "(Email not given)";
+                source_link = source_link ? source_link : "(Website not given)";
+                whatsapp_number = whatsapp_number ? whatsapp_number : "(Whatsapp number not given)";
+                org = org ? org : "(Company/ Organization name not given)";
+                relation = relation ? responseJSON.relation : "(Title not given)";
                 const pss_status = "01 PSS to be define";
                 
-                const replyMsg =
-`${salutation} ${name}
+                const replyMsg = `${salutation} ${name}
 ${email}  ${whatsapp_number}
 ${org}
 ${relation}
@@ -83,4 +80,4 @@ ${link}`;
             await msg.reply("An unexpected error occurred while handling your message.");
         }
     }
-})
+});
